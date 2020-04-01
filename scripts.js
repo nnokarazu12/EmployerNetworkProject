0de845b5c77e6a7971a520cdf0549c337490be58
nnokarazu12 <nnokarazu@gmail.com>
2020-03-31 20:39:43 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+﻿function toggleForm(event) {
+    var inputItems = document.querySelectorAll(".input-field");
+    for (var i = 0; i < inputItems.length; i++) {
+        inputItems[i].value = "";
+    }
+    var formSlider = document.getElementById('form-slider');
+    if (event.target.id == "signup-pointer") {
+        formSlider.style.left = "-400px";
+    } else {
+        formSlider.style.left = "0px";
+    }
+}
+function toggleCourseTab() {
+    //alert("h");
+    /*var courseTab = document.getElementById('course-tab');
+    if (courseTab.style.display == "none") {
+        courseTab.style.display = "block";
+    } else {
+        courseTab.style.display = "none";
+    }*/
+}
